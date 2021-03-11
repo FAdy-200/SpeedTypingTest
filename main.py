6c933a3b9d330c6226a918b091c3803932b48ebb
FAdy200 <abuyehia200@gmail.com>
2021-03-11 15:30:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import pygame
+import time
+import sys
+
+
+class SpeedTester:
+    def __init__(self):
+        """
+        initializes all the needed elements e.g pictures/time/pygame window
+        and all needed  attributes
+        """
+        pass
+
+    def __statistics(self):
+        """
+        calculates the statistics
+        :return:
+        """
+        pass
+
+    def __eventHandler(self):
+        """
+        handles the events that happen on screen e.g. mouse pos/clicks and user input
+        :return:
+        """
+        pass
+
+    def __typingChecker(self):
+        """
+        checks if the user has inputted the right characters
+        :return:
+        """
+        pass
+
+    def __initializeTypingTest(self):
+        """
+        resets the statistics and the typing challenge
+        :return:
+        """
+        pass
+
+    def __testDoneScreen(self):
+        """
+        renders the end screen with it is needed elements
+        :return:
+        """
+        pass
+
+    def __typingScreen(self):
+        """
+        renders the typing screen with it is needed elements
+        :return:
+        """
+        pass
+
+    def __mainScreen(self):
+        """
+        renders the main screen so the user does not just start typing right away
+        and there is a welcome screen
+        :return:
+        """
+        pass
+
+    def main(self):
+        """
+        main program loop
+        :return:
+        """
+        pass
+
+
+test = SpeedTester()
+test.main()
